09886255e791f53cc9df3068d0ba4af3157396ef
Alexandr Priezzhev <a@localhost.localdomain>
2015-01-08 15:07:21 +0200

    First commit

---
+++ web/hellocordova.dart
@@ -0,0 +1,23 @@
+import 'package:rikulo_gap/device.dart';
+import 'package:rikulo_gap/accelerometer.dart';
+
+//At a regular interval, get the acceleration along the x, y, and z axis.
+void accessAccelerometer() {
+	accelerometer.watchAcceleration(
+			(Acceleration acc) {
+				print("t:${acc.timestamp}, x:${acc.x}, y:${acc.y}, z:${acc.z}");
+			},
+			() => print("Fail to get acceleration."),
+			new AccelerometerOptions(frequency: 1000)
+	                                );
+}
+
+void main() {
+	Device.init()
+		.then((Device device) {
+			accessAccelerometer();
+		})
+		.catchError((ex, st) {
+			print("Failed: $ex, $st");
+		});
+}

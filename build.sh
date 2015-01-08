1fcd4598a78561790d3c4219d79d006d3ec0a866
Alexandr Priezzhev <a@priezz.net>
2015-01-08 17:43:50 +0200

    2015-01-17 17:43:50

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 # pub build --output=assets
 pub build
 # mv assets/web assets/www
-# rm
-# mv build/web www
+rm -rf www
+mv build/web www
+rm -rf build
 git add .
 git commit -m "$(date '+%Y-%m-%H %X')"
 git push
